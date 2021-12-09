3eb512c0b43b90431111588fe491c05f2eceb9d5
Ahmet Ertuğrul Özcan <ertugrul.ozcan@odeontechnology.com>
2021-12-09 10:29:52 +0300

    Query methods added for dynamic repositories

--- Ertis.MongoDB/Repository/IDynamicMongoRepository.cs
+++ Ertis.MongoDB/Repository/IDynamicMongoRepository.cs
@@ -1,9 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Ertis.Core.Collections;
 using Ertis.Data.Repository;
 
 namespace Ertis.MongoDB.Repository
 {
     public interface IDynamicMongoRepository : IDynamicRepository<string>
     {
-        
+        IPaginationCollection<dynamic> Query(
+            string query,
+            int? skip = null,
+            int? limit = null,
+            bool? withCount = null,
+            string sortField = null,
+            SortDirection? sortDirection = null,
+            IDictionary<string, bool> selectFields = null);
+
+        IPaginationCollection<dynamic> Query(
+            Expression<Func<dynamic, bool>> expression,
+            int? skip = null,
+            int? limit = null,
+            bool? withCount = null,
+            string sortField = null,
+            SortDirection? sortDirection = null,
+            IDictionary<string, bool> selectFields = null);
+
+        ValueTask<IPaginationCollection<dynamic>> QueryAsync(
+            string query,
+            int? skip = null,
+            int? limit = null,
+            bool? withCount = null,
+            string sortField = null,
+            SortDirection? sortDirection = null,
+            IDictionary<string, bool> selectFields = null);
+
+        ValueTask<IPaginationCollection<dynamic>> QueryAsync(
+            Expression<Func<dynamic, bool>> expression,
+            int? skip = null,
+            int? limit = null,
+            bool? withCount = null,
+            string sortField = null,
+            SortDirection? sortDirection = null,
+            IDictionary<string, bool> selectFields = null);
     }
 }

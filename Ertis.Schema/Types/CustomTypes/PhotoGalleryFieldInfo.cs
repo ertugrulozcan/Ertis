582ee7665f2911b30950ea5e29549faeb7516c1e
Ahmet Ertuğrul Özcan <ertugrul.ozcan@odeontechnology.com>
2023-02-11 22:16:31 +0300

    PhotoGalleryFieldInfo

--- Ertis.Schema/Types/CustomTypes/PhotoGalleryFieldInfo.cs
+++ Ertis.Schema/Types/CustomTypes/PhotoGalleryFieldInfo.cs
@@ -12,8 +12,13 @@ public class PhotoGalleryFieldInfo : FieldInfo<Array>
 {
 	#region Fields
 
+    private readonly int? maxSize;
     private readonly int? minCount;
     private readonly int? maxCount;
+    private readonly int? maxWidth;
+    private readonly int? maxHeight;
+    private readonly int? minWidth;
+    private readonly int? minHeight;
     private IFieldInfo itemSchema;
 
     #endregion
@@ -73,6 +78,21 @@ public class PhotoGalleryFieldInfo : FieldInfo<Array>
             return this.itemSchema;
         }
     }
+    
+    [JsonProperty("maxSize", NullValueHandling = NullValueHandling.Ignore)]
+    public int? MaxSize
+    {
+        get => this.maxSize;
+        init
+        {
+            this.maxSize = value;
+                
+            if (!this.ValidateMaxSize(out var exception))
+            {
+                throw exception;
+            }
+        }
+    }
 
     [JsonProperty("minCount", NullValueHandling = NullValueHandling.Ignore)]
     public int? MinCount
@@ -103,6 +123,81 @@ public class PhotoGalleryFieldInfo : FieldInfo<Array>
             }
         }
     }
+    
+    [JsonProperty("minWidth", NullValueHandling = NullValueHandling.Ignore)]
+    public int? MinWidth
+    {
+        get => this.minWidth;
+        init
+        {
+            this.minWidth = value;
+            
+            if (!this.ValidateMinWidth(out var exception))
+            {
+                throw exception;
+            }
+        }
+    }
+    
+    [JsonProperty("minHeight", NullValueHandling = NullValueHandling.Ignore)]
+    public int? MinHeight
+    {
+        get => this.minHeight;
+        init
+        {
+            this.minHeight = value;
+            
+            if (!this.ValidateMinHeight(out var exception))
+            {
+                throw exception;
+            }
+        }
+    }
+    
+    [JsonProperty("maxWidth", NullValueHandling = NullValueHandling.Ignore)]
+    public int? MaxWidth
+    {
+        get => this.maxWidth;
+        init
+        {
+            this.maxWidth = value;
+            
+            if (!this.ValidateMaxWidth(out var exception))
+            {
+                throw exception;
+            }
+        }
+    }
+    
+    [JsonProperty("maxHeight", NullValueHandling = NullValueHandling.Ignore)]
+    public int? MaxHeight
+    {
+        get => this.maxHeight;
+        init
+        {
+            this.maxHeight = value;
+            
+            if (!this.ValidateMaxHeight(out var exception))
+            {
+                throw exception;
+            }
+        }
+    }
+    
+    [JsonProperty("recommendedWidth", NullValueHandling = NullValueHandling.Ignore)]
+    public int? RecommendedWidth { get; set; }
+    
+    [JsonProperty("recommendedHeight", NullValueHandling = NullValueHandling.Ignore)]
+    public int? RecommendedHeight { get; set; }
+    
+    [JsonProperty("maxSizesRequired")]
+    public bool MaxSizesRequired { get; set; }
+    
+    [JsonProperty("minSizesRequired")]
+    public bool MinSizesRequired { get; set; }
+    
+    [JsonProperty("aspectRatioRequired")]
+    public bool AspectRatioRequired { get; set; }
 
     #endregion
 
@@ -113,6 +208,7 @@ public class PhotoGalleryFieldInfo : FieldInfo<Array>
         base.ValidateSchema(out exception);
         this.ValidateMinCount(out exception);
         this.ValidateMaxCount(out exception);
+        this.ValidateMaxSize(out exception);
 
         return exception == null;
     }
@@ -144,6 +240,18 @@ public class PhotoGalleryFieldInfo : FieldInfo<Array>
         }
         
         return isValid;
+    }
+    
+    private bool ValidateMaxSize(out Exception exception)
+    {
+        if (this.MaxSize < 0)
+        {
+            exception = new FieldValidationException("MaxSize can not be less than zero", this);
+            return false;
+        }
+            
+        exception = null;
+        return true;
     }
 
     private bool ValidateMinCount(out Exception exception)
@@ -188,6 +296,90 @@ public class PhotoGalleryFieldInfo : FieldInfo<Array>
         return true;
     }
     
+    private bool ValidateMinWidth(out Exception exception)
+    {
+        if (this.MinWidth != null)
+        {
+            if (this.MinWidth < 0)
+            {
+                exception = new FieldValidationException($"The 'minWidth' value can not be less than zero ('{this.Name}')", this);
+                return false;
+            }
+
+            if (this.MaxWidth != null && this.MinWidth != null && this.MaxWidth < this.MinWidth)
+            {
+                exception = new FieldValidationException($"The 'minWidth' value can not be greater than the 'maxWidth' value ('{this.Name}')", this);
+                return false;
+            }
+        }
+        
+        exception = null;
+        return true;
+    }
+    
+    private bool ValidateMinHeight(out Exception exception)
+    {
+        if (this.MinHeight != null)
+        {
+            if (this.MinHeight < 0)
+            {
+                exception = new FieldValidationException($"The 'minHeight' value can not be less than zero ('{this.Name}')", this);
+                return false;
+            }
+
+            if (this.MaxHeight != null && this.MinHeight != null && this.MaxHeight < this.MinHeight)
+            {
+                exception = new FieldValidationException($"The 'minHeight' value can not be greater than the 'maxHeight' value ('{this.Name}')", this);
+                return false;
+            }
+        }
+        
+        exception = null;
+        return true;
+    }
+    
+    private bool ValidateMaxWidth(out Exception exception)
+    {
+        if (this.MaxWidth != null)
+        {
+            if (this.MaxWidth < 0)
+            {
+                exception = new FieldValidationException($"The 'maxWidth' value can not be less than zero ('{this.Name}')", this);
+                return false;
+            }
+
+            if (this.MinWidth != null && this.MaxWidth != null && this.MinWidth > this.MaxWidth)
+            {
+                exception = new FieldValidationException($"The 'minWidth' value can not be greater than the 'maxWidth' value ('{this.Name}')", this);
+                return false;
+            }
+        }
+        
+        exception = null;
+        return true;
+    }
+    
+    private bool ValidateMaxHeight(out Exception exception)
+    {
+        if (this.MaxHeight != null)
+        {
+            if (this.MaxHeight < 0)
+            {
+                exception = new FieldValidationException($"The 'maxHeight' value can not be less than zero ('{this.Name}')", this);
+                return false;
+            }
+
+            if (this.MinHeight != null && this.MaxHeight != null && this.MinHeight > this.MaxHeight)
+            {
+                exception = new FieldValidationException($"The 'minHeight' value can not be greater than the 'maxHeight' value ('{this.Name}')", this);
+                return false;
+            }
+        }
+        
+        exception = null;
+        return true;
+    }
+    
     public override object Clone()
     {
         return new PhotoGalleryFieldInfo
@@ -201,8 +393,18 @@ public class PhotoGalleryFieldInfo : FieldInfo<Array>
             IsHidden = this.IsHidden,
             IsReadonly = this.IsReadonly,
             DefaultValue = this.DefaultValue,
+            MaxSize = this.MaxSize,
             MinCount = this.MinCount,
-            MaxCount = this.MaxCount
+            MaxCount = this.MaxCount,
+            MinWidth = this.MinWidth,
+            MinHeight = this.MinHeight,
+            MaxWidth = this.MaxWidth,
+            MaxHeight = this.MaxHeight,
+            RecommendedWidth = this.RecommendedWidth,
+            RecommendedHeight = this.RecommendedHeight,
+            MinSizesRequired = this.MinSizesRequired,
+            MaxSizesRequired = this.MaxSizesRequired,
+            AspectRatioRequired = this.AspectRatioRequired
         };
     }
 

cfd92d606b5a2f13f693d02c6c1f0c7df287c892
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2025-02-06 01:08:08 +0300

    QueryString & HeaderCollection empty instances are immutable now

--- Ertis.Net/Http/HeaderCollection.cs
+++ Ertis.Net/Http/HeaderCollection.cs
@@ -1,143 +1,153 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
-namespace Ertis.Net.Http
+namespace Ertis.Net.Http;
+
+// ReSharper disable once UnusedType.Global
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public static class HeaderCollection
 {
-	public static class HeaderCollection
+	#region Statics
+	
+	public static IHeaderCollection Empty => new RequestHeaders { IsImmutable = true };
+	
+	#endregion
+	
+	#region Methods
+	
+	public static IHeaderCollection Create()
 	{
-		#region Statics
-
-		public static readonly IHeaderCollection Empty = new RequestHeaders();
-
-		#endregion
-
-		#region Methods
-
-		public static IHeaderCollection Add(string key, object value)
-		{
-			return new RequestHeaders(new Dictionary<string, object> { { key, value } });
-		}
-		
-		public static IHeaderCollection Add(IHeaderCollection collection)
-		{
-			return new RequestHeaders(collection.ToDictionary());
-		}
-
-		#endregion
+		return new RequestHeaders();
 	}
 	
-	public class RequestHeaders : IHeaderCollection
+	public static IHeaderCollection Add(string key, object value)
 	{
-		#region Properties
-
-		private Dictionary<string, object> HeadersDictionary { get; }
-
-		public IEnumerable<string> Keys => this.HeadersDictionary.Keys;
-		
-		public IEnumerable<object> Values => this.HeadersDictionary.Values;
-
-		#endregion
-		
-		#region Constructors
-
-		/// <summary>
-		/// Constructor 1
-		/// </summary>
-		internal RequestHeaders()
-		{
-			this.HeadersDictionary = new Dictionary<string, object>();
-		}
-		
-		/// <summary>
-		/// Constructor 2
-		/// </summary>
-		/// <param name="dictionary"></param>
-		internal RequestHeaders(IDictionary<string, object> dictionary) : this()
-		{
-			foreach (var pair in dictionary)
-			{
-				this.HeadersDictionary.Add(pair.Key, pair.Value);
-			}
-		}
-
-		#endregion
-
-		#region Methods
-
-		public IHeaderCollection Add(string key, object value)
-		{
-			if (string.IsNullOrEmpty(key))
-			{
-				throw new ArgumentException("Header key is empty!");
-			}
-
-			if (value == null || string.IsNullOrEmpty(value.ToString()))
-			{
-				throw new ArgumentException("Header value is null or empty!");
-			}
-			
-			if (!this.HeadersDictionary.ContainsKey(key))
-				this.HeadersDictionary.Add(key, value);
-			else
-				this.HeadersDictionary[key] = value;
-			
-			return this;
-		}
-
-		public IHeaderCollection Add(KeyValuePair<string, object> pair)
-		{
-			return this.Add(pair.Key, pair.Value);
-		}
-		
-		public IHeaderCollection Add(IHeaderCollection collection)
-		{
-			IHeaderCollection headers = this;
-			var dictionary = collection.ToDictionary();
-			foreach (var pair in dictionary)
-			{
-				headers = this.Add(pair.Key, pair.Value);
-			}
-
-			return headers;
-		}
-
-		public IHeaderCollection Remove(string key)
-		{
-			if (this.HeadersDictionary.ContainsKey(key))
-			{
-				this.HeadersDictionary.Remove(key);
-			}
-
-			return this;
-		}
-
-		public IDictionary<string, object> ToDictionary()
-		{
-			return this.HeadersDictionary;
-		}
-
-		public bool ContainsKey(string key)
-		{
-			return this.HeadersDictionary.ContainsKey(key);
-		}
-
-		public override string ToString()
-		{
-			return string.Join("&", 
-				this.HeadersDictionary
-					.Where(x => !string.IsNullOrEmpty(x.Key))
-					.Select(x => $"{x.Key}={Uri.EscapeDataString(x.Value.ToString() ?? "")}"));
-		}
-
-		public IEnumerator GetEnumerator()
-		{
-			return this.HeadersDictionary.GetEnumerator();
-		}
-
-		IEnumerator<object> IEnumerable<object>.GetEnumerator()
-		{
-			return this.HeadersDictionary.Values.GetEnumerator();
-		}
-
-		#endregion
+		return new RequestHeaders(new Dictionary<string, object> { { key, value } });
 	}
+	
+	public static IHeaderCollection Add(IHeaderCollection collection)
+	{
+		return new RequestHeaders(collection.ToDictionary());
+	}
+	
+	#endregion
+}
+
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+public class RequestHeaders : IHeaderCollection
+{
+	#region Properties
+	
+	internal bool IsImmutable { get; init; }
+	
+	private Dictionary<string, object> HeadersDictionary { get; }
+	
+	public IEnumerable<string> Keys => this.HeadersDictionary.Keys;
+	
+	public IEnumerable<object> Values => this.HeadersDictionary.Values;
+	
+	#endregion
+	
+	#region Constructors
+	
+	/// <summary>
+	/// Constructor 1
+	/// </summary>
+	internal RequestHeaders()
+	{
+		this.HeadersDictionary = new Dictionary<string, object>();
+	}
+	
+	/// <summary>
+	/// Constructor 2
+	/// </summary>
+	/// <param name="dictionary"></param>
+	internal RequestHeaders(IDictionary<string, object> dictionary) : this()
+	{
+		foreach (var pair in dictionary)
+		{
+			this.HeadersDictionary.Add(pair.Key, pair.Value);
+		}
+	}
+	
+	#endregion
+	
+	#region Methods
+	
+	public IHeaderCollection Add(string key, object value)
+	{
+		if (string.IsNullOrEmpty(key))
+		{
+			throw new ArgumentException("Header key is empty!");
+		}
+		
+		if (value == null || string.IsNullOrEmpty(value.ToString()))
+		{
+			throw new ArgumentException("Header value is null or empty!");
+		}
+		
+		if (this.IsImmutable)
+		{
+			return new RequestHeaders { { key, value } };
+		}
+		else
+		{
+			this.HeadersDictionary[key] = value;
+			return this;
+		}
+	}
+	
+	public IHeaderCollection Add(KeyValuePair<string, object> pair)
+	{
+		return this.Add(pair.Key, pair.Value);
+	}
+	
+	public IHeaderCollection Add(IHeaderCollection collection)
+	{
+		IHeaderCollection headers = this;
+		var dictionary = collection.ToDictionary();
+		foreach (var pair in dictionary)
+		{
+			headers = headers.Add(pair.Key, pair.Value);
+		}
+		
+		return headers;
+	}
+	
+	public IHeaderCollection Remove(string key)
+	{
+        this.HeadersDictionary.Remove(key);
+        return this;
+	}
+	
+	public IDictionary<string, object> ToDictionary()
+	{
+		return this.HeadersDictionary;
+	}
+	
+	public bool ContainsKey(string key)
+	{
+		return this.HeadersDictionary.ContainsKey(key);
+	}
+	
+	public override string ToString()
+	{
+		return string.Join("&", 
+			this.HeadersDictionary
+				.Where(x => !string.IsNullOrEmpty(x.Key))
+				.Select(x => $"{x.Key}={Uri.EscapeDataString(x.Value.ToString() ?? "")}"));
+	}
+	
+	public IEnumerator GetEnumerator()
+	{
+		return this.HeadersDictionary.GetEnumerator();
+	}
+	
+	IEnumerator<object> IEnumerable<object>.GetEnumerator()
+	{
+		return this.HeadersDictionary.Values.GetEnumerator();
+	}
+	
+	#endregion
 }
--- Ertis.Net/Http/QueryString.cs
+++ Ertis.Net/Http/QueryString.cs
@@ -1,139 +1,149 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
-namespace Ertis.Net.Http
+namespace Ertis.Net.Http;
+
+// ReSharper disable once UnusedType.Global
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public static class QueryString
 {
-	public static class QueryString
+	#region Statics
+	
+	public static IQueryString Empty => new HttpQueryString { IsImmutable = true };
+	
+	#endregion
+	
+	#region Methods
+	
+	public static IQueryString Create()
 	{
-		#region Statics
-
-		public static readonly IQueryString Empty = new HttpQueryString();
-
-		#endregion
-		
-		#region Methods
-
-		public static IQueryString Add(string key, object value)
-		{
-			return new HttpQueryString(new Dictionary<string, object> { { key, value } });
-		}
-		
-		public static IQueryString Add(IQueryString collection)
-		{
-			return new HttpQueryString(collection.ToDictionary());
-		}
-
-		#endregion
+		return new HttpQueryString();
 	}
 	
-	public class HttpQueryString : IQueryString
+	public static IQueryString Add(string key, object value)
 	{
-		#region Properties
-
-		private Dictionary<string, object> QueryDictionary { get; }
-
-		#endregion
-		
-		#region Constructors
-
-		/// <summary>
-		/// Constructor 1
-		/// </summary>
-		internal HttpQueryString()
-		{
-			this.QueryDictionary = new Dictionary<string, object>();
-		}
-		
-		/// <summary>
-		/// Constructor 2
-		/// </summary>
-		/// <param name="dictionary"></param>
-		internal HttpQueryString(IDictionary<string, object> dictionary) : this()
-		{
-			foreach (var pair in dictionary)
-			{
-				this.QueryDictionary.Add(pair.Key, pair.Value);
-			}
-		}
-
-		#endregion
-
-		#region Methods
-
-		public IQueryString Add(string key, object value)
-		{
-			if (string.IsNullOrEmpty(key))
-			{
-				throw new ArgumentException("QueryString key is empty!");
-			}
-
-			if (value == null || string.IsNullOrEmpty(value.ToString()))
-			{
-				throw new ArgumentException("QueryString value is null or empty!");
-			}
-			
-			if (!this.QueryDictionary.ContainsKey(key))
-				this.QueryDictionary.Add(key, value);
-			else
-				this.QueryDictionary[key] = value;
-			
-			return this;
-		}
-
-		public IQueryString Add(KeyValuePair<string, object> pair)
-		{
-			return this.Add(pair.Key, pair.Value);
-		}
-		
-		public IQueryString Add(IQueryString collection)
-		{
-			IQueryString queryString = this;
-			var dictionary = collection.ToDictionary();
-			foreach (var pair in dictionary)
-			{
-				queryString = this.Add(pair.Key, pair.Value);
-			}
-
-			return queryString;
-		}
-
-		public IQueryString Remove(string key)
-		{
-			if (this.QueryDictionary.ContainsKey(key))
-			{
-				this.QueryDictionary.Remove(key);
-			}
-
-			return this;
-		}
-
-		public IDictionary<string, object> ToDictionary()
-		{
-			return this.QueryDictionary;
-		}
-
-		public bool ContainsKey(string key)
-		{
-			return this.QueryDictionary.ContainsKey(key);
-		}
-
-		public override string ToString()
-		{
-			return string.Join("&", 
-				this.QueryDictionary
-					.Where(x => !string.IsNullOrEmpty(x.Key))
-					.Select(x => $"{x.Key}={Uri.EscapeDataString(x.Value.ToString() ?? "")}"));
-		}
-
-		public IEnumerator GetEnumerator()
-		{
-			return this.QueryDictionary.GetEnumerator();
-		}
-
-		IEnumerator<object> IEnumerable<object>.GetEnumerator()
-		{
-			return this.QueryDictionary.Values.GetEnumerator();
-		}
-
-		#endregion
+		return new HttpQueryString(new Dictionary<string, object> { { key, value } });
 	}
+	
+	public static IQueryString Add(IQueryString collection)
+	{
+		return new HttpQueryString(collection.ToDictionary());
+	}
+	
+	#endregion
+}
+
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+public class HttpQueryString : IQueryString
+{
+	#region Properties
+	
+	internal bool IsImmutable { get; init; }
+	
+	private Dictionary<string, object> QueryDictionary { get; }
+	
+	#endregion
+	
+	#region Constructors
+	
+	/// <summary>
+	/// Constructor 1
+	/// </summary>
+	internal HttpQueryString()
+	{
+		this.QueryDictionary = new Dictionary<string, object>();
+	}
+	
+	/// <summary>
+	/// Constructor 2
+	/// </summary>
+	/// <param name="dictionary"></param>
+	internal HttpQueryString(IDictionary<string, object> dictionary) : this()
+	{
+		foreach (var pair in dictionary)
+		{
+			this.QueryDictionary.Add(pair.Key, pair.Value);
+		}
+	}
+	
+	#endregion
+	
+	#region Methods
+	
+	public IQueryString Add(string key, object value)
+	{
+		if (string.IsNullOrEmpty(key))
+		{
+			throw new ArgumentException("QueryString key is empty!");
+		}
+		
+		if (value == null || string.IsNullOrEmpty(value.ToString()))
+		{
+			throw new ArgumentException("QueryString value is null or empty!");
+		}
+		
+		if (this.IsImmutable)
+		{
+			return new HttpQueryString { { key, value } };
+		}
+		else
+		{
+			this.QueryDictionary[key] = value;
+			return this;
+		}
+	}
+	
+	public IQueryString Add(KeyValuePair<string, object> pair)
+	{
+		return this.Add(pair.Key, pair.Value);
+	}
+	
+	public IQueryString Add(IQueryString collection)
+	{
+		IQueryString queryString = this;
+		var dictionary = collection.ToDictionary();
+		foreach (var pair in dictionary)
+		{
+			queryString = queryString.Add(pair.Key, pair.Value);
+		}
+		
+		return queryString;
+	}
+	
+	public IQueryString Remove(string key)
+	{
+        this.QueryDictionary.Remove(key);
+        return this;
+	}
+	
+	public IDictionary<string, object> ToDictionary()
+	{
+		return this.QueryDictionary;
+	}
+	
+	public bool ContainsKey(string key)
+	{
+		return this.QueryDictionary.ContainsKey(key);
+	}
+	
+	public override string ToString()
+	{
+		return string.Join("&", 
+			this.QueryDictionary
+				.Where(x => !string.IsNullOrEmpty(x.Key))
+				.Select(x => $"{x.Key}={Uri.EscapeDataString(x.Value.ToString() ?? "")}"));
+	}
+	
+	public IEnumerator GetEnumerator()
+	{
+		return this.QueryDictionary.GetEnumerator();
+	}
+	
+	IEnumerator<object> IEnumerable<object>.GetEnumerator()
+	{
+		return this.QueryDictionary.Values.GetEnumerator();
+	}
+	
+	#endregion
 }

a176af01d966557f59a60593a68b94d1d98f94e0
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2025-06-16 00:35:38 +0300

    Quality option fixed

--- Ertis.ImageProcessing/FormatEncoder.cs
+++ Ertis.ImageProcessing/FormatEncoder.cs
@@ -14,18 +14,18 @@ public static class FormatEncoder
 {
 	#region Methods
 
-	public static IImageEncoder GetDefaultFormatter(ImageFormat format)
+	public static IImageEncoder GetDefaultFormatter(ImageFormat format, int? quality = null)
 	{
 		return format switch
 		{
 			ImageFormat.Bmp => new BmpEncoder(),
 			ImageFormat.Gif => new GifEncoder(),
-			ImageFormat.Jpeg => new JpegEncoder(),
+			ImageFormat.Jpeg => new JpegEncoder { Quality = quality ?? Constants.DefaultQuality },
 			ImageFormat.Pbm => new PbmEncoder(),
 			ImageFormat.Png => new PngEncoder(),
 			ImageFormat.Tga => new TgaEncoder(),
 			ImageFormat.Tiff => new TiffEncoder(),
-			ImageFormat.Webp => new WebpEncoder(),
+			ImageFormat.Webp => new WebpEncoder { Quality = quality ?? Constants.DefaultQuality },
 			_ => throw new ArgumentOutOfRangeException(nameof(format), format, null)
 		};
 	}

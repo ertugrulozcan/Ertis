2c92c0a5299e7432f89c8ec7600db3bb2d01ff55
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2025-09-17 12:44:11 +0300

    RawRequestBody implemented

---
+++ Ertis.Net/Http/RawRequestBody.cs
@@ -0,0 +1,51 @@
+using System.Net.Http.Headers;
+
+namespace Ertis.Net.Http;
+
+public class RawRequestBody : IRequestBody
+{
+    #region Properties
+    
+    public object? Payload { get; }
+    
+    public BodyTypes Type { get; }
+    
+    private string ContentType { get; }
+    
+    #endregion
+    
+    #region Constructors
+    
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="payload"></param>
+    /// <param name="bodyType"></param>
+    /// <param name="contentType"></param>
+    public RawRequestBody(byte[] payload, BodyTypes bodyType, string contentType)
+    {
+        this.Payload = payload;
+        this.Type = bodyType;
+        this.ContentType = contentType;
+    }
+    
+    #endregion
+    
+    #region Methods
+    
+    public HttpContent GetHttpContent()
+    {
+        if (this.Payload is byte[] buffer)
+        {
+            var content = new ByteArrayContent(buffer);
+            content.Headers.ContentType = new MediaTypeHeaderValue(this.ContentType);
+            return content;	
+        }
+        else
+        {
+            return new StringContent(string.Empty);
+        }
+    }
+    
+    #endregion
+}

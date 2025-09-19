c5e28ba8153e01a92dbcbd66c20e624cc7d913f8
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2025-09-19 14:40:30 +0300

    Index name fixed

--- Ertis.MongoDB/Repository/MongoRepositoryBase.cs
+++ Ertis.MongoDB/Repository/MongoRepositoryBase.cs
@@ -263,6 +263,7 @@ namespace Ertis.MongoDB.Repository
 			{
 				var indexOptions = new CreateIndexOptions
 				{
+					Name = indexDefinition.Key, 
 					DefaultLanguage = indexDefinition.Locale.ToString()
 				};
 				

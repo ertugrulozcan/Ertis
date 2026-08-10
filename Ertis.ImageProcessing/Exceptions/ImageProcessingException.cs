c49f21ba7e04344619667ba1e8bd8f702effb179
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2026-08-10 19:42:19 +0300

    Update ImageProcessingException.cs

--- Ertis.ImageProcessing/Exceptions/ImageProcessingException.cs
+++ Ertis.ImageProcessing/Exceptions/ImageProcessingException.cs
@@ -3,12 +3,35 @@ using Ertis.Core.Exceptions;
 
 namespace Ertis.ImageProcessing.Exceptions;
 
-public class ImageProcessingException(
-	HttpStatusCode httpStatusCode,
-	string? message = null,
-	string? errorCode = null,
-	Exception? exception = null)
-	: ErtisException(httpStatusCode,
-		message,
-		errorCode ?? "ImageProcessingException",
-		exception);
+public class ImageProcessingException : ErtisException
+{
+	#region Constructors
+	
+	/// <summary>
+	/// Constructor 1
+	/// </summary>
+	/// <param name="httpStatusCode"></param>
+	/// <param name="message"></param>
+	/// <param name="errorCode"></param>
+	public ImageProcessingException(
+		HttpStatusCode httpStatusCode,
+		string message,
+		string errorCode) : base(httpStatusCode, message, errorCode)
+	{ }
+	
+	/// <summary>
+	/// Constructor 2
+	/// </summary>
+	/// <param name="httpStatusCode"></param>
+	/// <param name="message"></param>
+	/// <param name="errorCode"></param>
+	/// <param name="exception"></param>
+	public ImageProcessingException(
+		HttpStatusCode httpStatusCode,
+		string message,
+		string errorCode,
+		Exception exception) : base(httpStatusCode, message, errorCode, exception)
+	{ }
+	
+	#endregion
+}

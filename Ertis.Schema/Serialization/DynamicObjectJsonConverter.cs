b95dd159a00a39be774cf211bfadcbe83015d55d
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2026-09-23 19:38:59 +0300

    DynamicObjectJsonConverter fixed

--- Ertis.Schema/Serialization/DynamicObjectJsonConverter.cs
+++ Ertis.Schema/Serialization/DynamicObjectJsonConverter.cs
@@ -9,15 +9,27 @@ public class DynamicObjectJsonConverter : JsonConverter<DynamicObject>
 {
 	#region Methods
 	
+	public override bool CanConvert(Type typeToConvert)
+	{
+		return typeof(DynamicObject).IsAssignableFrom(typeToConvert);
+	}
+	
 	public override DynamicObject? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 	{
-		var json = reader.GetString();
+		if (reader.TokenType == JsonTokenType.Null)
+		{
+			return null;
+		}
+		
+		using var document = JsonDocument.ParseValue(ref reader);
+		var json = document.RootElement.GetRawText();
+		
 		return string.IsNullOrEmpty(json) ? null : DynamicObject.Parse(json);
 	}
 	
 	public override void Write(Utf8JsonWriter writer, DynamicObject dynamicObject, JsonSerializerOptions options)
 	{
-		writer.WriteStringValue(dynamicObject.ToJson());
+		writer.WriteRawValue(dynamicObject.ToJson(), skipInputValidation: false);
 	}
 	
 	#endregion
